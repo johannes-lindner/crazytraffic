5e0a011e471fa2a33393ee6b6949de29bd4c3b0f
Johannes Lindner <82418366+johannes-lindner@users.noreply.github.com>
2024-08-13 19:02:27 +0200

    update gridsystem

---
+++ Assets/GridSystem/GridSystem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour
+{
+    public string datapath = Application.dataPath.ToString() + "/Data";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
